4db79cf69fc4c7550a67175f93bff2a0092f3ae5
mihailSpaseski <spaseskimihail@gmail.com>
2023-11-11 21:27:55 +0100

    router implement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutusComponent } from './home/aboutus/aboutus.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './home/contact-us/contact-us.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'home', component: AppComponent },
+  { path: 'about-us', component: AboutusComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutusComponent,
+    HomeComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forChild(appRoutes),
   ],
   providers: [],
-  bootstrap: [AppComponent, NavbarComponent]
+  bootstrap: [AppComponent, NavbarComponent, AboutusComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }
