c4e58563be380ace3c245ded9d5da02120d79b63
mihailSpaseski <spaseskimihail@gmail.com>
2024-02-01 00:44:58 +0100

    json file with countries and oppening the company register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from '../../services/authentication.service';
 import { Router } from '@angular/router';
@@ -9,7 +9,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
   hide: boolean = true;
   hideConfirmPassword: boolean = true;
@@ -21,11 +21,37 @@ export class RegisterComponent {
     confirmPassword: new FormControl('', [Validators.required]),
   })
 
+  companyRegisterForm: FormGroup = new FormGroup({
+    companyName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
+    fullName: new FormControl('', [Validators.required]),
+    enterCountry: new FormControl('', [Validators.required]),
+    emailAddress: new FormControl('', [Validators.required, Validators.email]),
+    phoneNumberWithAreaCode: new FormControl('', [Validators.required]),
+    productWantToBuy: new FormControl('', [Validators.required])
+  })
+
   constructor(
     private authService: AuthenticationService,
     private router: Router,
     private afAuth: AngularFireAuth
   ) { }
+
+  ngOnInit(): void {
+
+    // async function logDataFromApi() {
+    //   try {
+    //     const response = await fetch('https://restcountries.com/v3.1/all');
+    //     const data = await response.json();
+    //     const countries = data.map((item: any) => item.name.common);
+    //     console.log(countries)
+    //   } catch (error) {
+    //     console.error('Error fetching data from API:', error);
+    //   }
+    // }
+    // logDataFromApi(); 
+
+  }
+
 
   registerWithEmailPassword() {
 
